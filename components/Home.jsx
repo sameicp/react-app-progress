6afcafddf8b1e1cae0ea4888d6ff6580a8d132cf
sameicp <same.eth2001@gmail.com>
2023-12-03 00:09:07 +0200

    introduction to useEffect

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 export default function Home() {
@@ -8,15 +8,22 @@ export default function Home() {
         {title: "Her Name Is", body: 'Tanatswa is cool', author: 'Tanatswa', id: 2},
         {title: "His Name Is", body: 'Tafadzwa is cool', author: 'Tafadzwa', id: 3}
     ])
+    const [name, setName] = useState('Samuel');
 
     const handleDelete = (id) =>{
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(()=>{
+        console.log('Name: '+name)
+    }, [name])
     
   return (
     <div className="home">
         <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+        <button onClick={()=>setName('Tanatswa')}>change name</button>
+        <p>{name}</p>
     </div>
   )
 }
