3f64e0c9dee9d01deb14964a2e2a59335d1c7bad
sameicp <same.eth2001@gmail.com>
2023-12-03 22:41:45 +0200

    intro to react router

---
+++ components/Create.jsx
@@ -0,0 +1,6 @@
+
+export default function Create() {
+  return (
+    <div>Create New Blog</div>
+  )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 
 import Navbar from '../components/Navbar'
 import Home from '../components/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Create from '../components/Create'
 
 function App() {
   
   return (
-
+    <Router>
     <div className='App'>
       <Navbar />
       <div className='content'>
-        <Home/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/create' element={<Create/>} />
+        </Routes>
       </div>
     </div>
+    </Router>
   )
 }
 
